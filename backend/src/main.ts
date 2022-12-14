618f16749a5e105d93a2b09404e0ca47c942aff2
mohamedadel95 <105986572+mohamedadel95@users.noreply.github.com>
2022-12-15 01:50:16 +0200

    test stage

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,6 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
